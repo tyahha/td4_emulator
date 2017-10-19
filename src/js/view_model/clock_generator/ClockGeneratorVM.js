cc1ad167072237984dea143d76bd0626f3f84c6c
佐藤利徳 <tyahhahha@gmail.com>
2017-10-20 02:46:15 +0900

    ソースの追加漏れ

---
+++ src/js/view_model/clock_generator/ClockGeneratorVM.js
@@ -0,0 +1,53 @@
+import ko from 'knockout'
+
+const ClockMode = {
+  _1Hz: '1Hz',
+  _10Hz: '10Hz',
+  Manual: 'Manual',
+}
+
+export default class ClockGeneratorVM {
+  constructor(clockHandler, resetHandler) {
+    this.clockHandler = clockHandler
+    this.resetHandler = resetHandler
+    this.mode = ko.observable(ClockMode.Manual)
+
+    setInterval(() => {
+      if (this.is1Hz()) {
+        this.clockHandler()
+      }
+    }, 1000)
+
+    setInterval(() => {
+      if (this.is10Hz()) {
+        this.clockHandler()
+      }
+    }, 100)
+  }
+
+  currentMode() {
+    return this.mode._latestValue
+  }
+
+  is1Hz() {
+    return this.currentMode() === ClockMode._1Hz
+  }
+
+  is10Hz() {
+    return this.currentMode() === ClockMode._10Hz
+  }
+
+  isManual() {
+    return this.currentMode() === ClockMode.Manual
+  }
+
+  clock() {
+    if (this.isManual()) {
+      this.clockHandler()
+    }
+  }
+
+  reset() {
+    this.resetHandler()
+  }
+}
