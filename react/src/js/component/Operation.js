f0ccfb4efa51a698d761bef11e03c38945d66e28
佐藤利徳 <tyahhahha@gmail.com>
2018-01-19 23:51:24 +0900

    react operationをすべて実装

--- react/src/js/component/Operation.js
+++ react/src/js/component/Operation.js
@@ -18,17 +18,17 @@ export function operate(state) {
 function getOperator(operatorNumber) {
   switch (operatorNumber) {
     case  0: return addA
-    // case  1: return moveAB
-    // case  2: return inputA
-    // case  3: return moveA
-    // case  4: return moveBA
+    case  1: return moveAB
+    case  2: return inputA
+    case  3: return moveA
+    case  4: return moveBA
     case  5: return addB
-    // case  6: return inputB
-    // case  7: return moveB
-    // case  9: return outputB
-    // case 11: return outputData
-    // case 14: return jumpIf
-    // case 15: return jump
+    case  6: return inputB
+    case  7: return moveB
+    case  9: return outputB
+    case 11: return outputData
+    case 14: return jumpIf
+    case 15: return jump
     default:
       console.log(`unknouwn operator ${operatorNumber}`)
       return unknown
@@ -59,5 +59,83 @@ function addB(state, operand) {
     programCount: state.programCount + 1,
     carry,
     registorB: carry ? addResult - 16 : addResult,
+  })
+}
+
+function moveAB(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorA: state.registorB,
+  })
+}
+
+function moveBA(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorB: state.registorA,
+  })
+}
+
+function moveA(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorA: operand,
+  })
+}
+
+function moveB(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorB: operand,
+  })
+}
+
+function inputA(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorA: state.input,
+  })
+}
+
+function inputB(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    registorB: state.input,
+  })
+}
+
+function outputB(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    output: state.registorB,
+  })
+}
+
+function outputData(state, operand) {
+  return Object.assign(state, {
+    programCount: state.programCount + 1,
+    carry: false,
+    output: operand,
+  })
+}
+
+function jump(state, operand) {
+  return Object.assign(state, {
+    programCount: operand,
+    carry: false,
+  })
+}
+
+function jumpIf(state, operand) {
+  return Object.assign(state, {
+    programCount: state.carry ? state.programCount + 1 : operand,
+    carry: false,
   })
 }
